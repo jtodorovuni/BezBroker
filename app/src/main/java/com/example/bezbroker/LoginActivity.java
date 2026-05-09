8620132d293c76c5f86aa654a5aefad07f772393
fmi <jtodorv@uni-plovdiv.bg>
2026-05-09 12:16:09 +0300

    Commit before the break.

--- app/src/main/java/com/example/bezbroker/LoginActivity.java
+++ app/src/main/java/com/example/bezbroker/LoginActivity.java
@@ -1,6 +1,12 @@
 package com.example.bezbroker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +16,37 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText emailET;
+    EditText passwordET;
+    TextView registerTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        emailET = findViewById(R.id.loginEmailET);
+        passwordET = findViewById(R.id.loginPassowordET);
+        registerTV = findViewById(R.id.registerTV);
+
+        registerTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
         });
+    }
+
+    public void onLoginClick(View view){
+        String email = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        if(email.length() == 0 || password.length() == 0){
+            Toast.makeText(this, R.string.error_message_empty_fields, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //request to the server
     }
 }
--- app/src/main/java/com/example/bezbroker/RegisterActivity.java
+++ app/src/main/java/com/example/bezbroker/RegisterActivity.java
@@ -1,6 +1,11 @@
 package com.example.bezbroker;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,51 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText fNameET;
+    EditText sNameET;
+    EditText emailET;
+    EditText passwordET;
+    EditText repeatPasswordET;
+    EditText phoneET;
+    CheckBox sellerCB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        fNameET = findViewById(R.id.registerFNameET);
+        sNameET = findViewById(R.id.registerSNameET);
+        emailET = findViewById(R.id.registerEmailET);
+        passwordET = findViewById(R.id.registerPasswordET);
+        repeatPasswordET = findViewById(R.id.registerRepeatPasswordET);
+        phoneET = findViewById(R.id.registerPhoneET);
+        sellerCB = findViewById(R.id.registerCB);
+    }
+
+    public void onCancelClick(View view){
+        finish();
+    }
+
+    public void onRegisterClick(View view){
+
+        if(emailET.getText().length() == 0 || passwordET.getText().length() == 0){
+            Toast.makeText(this, R.string.error_message_empty_fields, Toast.LENGTH_SHORT).show();
+            return;
+        }else if(!passwordET.getText().toString().equals(repeatPasswordET.getText().toString())){
+            Toast.makeText(this, R.string.error_password_missmatch, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String email = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+        String firstName = fNameET.getText().toString();
+        String lastName = sNameET.getText().toString();
+        String phone = phoneET.getText().toString();
+        boolean isSeller = sellerCB.isChecked();
+
+        // send data to the server and see the response
+
     }
 }
